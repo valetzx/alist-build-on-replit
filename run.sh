8fab77c84ce7d0577f410c9247ce7f35b0dad251
valetzx <45054400+valetzx@users.noreply.github.com>
2022-04-19 10:57:15 +0800

    Create run.sh

---
+++ run.sh
@@ -0,0 +1,15 @@
+appName="alist"
+builtAt="$(date +'%F %T %z')"
+goVersion=$(go version | sed 's/go version //')
+gitAuthor=$(git show -s --format='format:%aN <%ae>' HEAD)
+gitCommit=$(git log --pretty=format:"%h" -1)
+gitTag=$(git describe --long --tags --dirty --always)
+ldflags="\
+-w -s \
+-X 'github.com/Xhofe/alist/conf.BuiltAt=$builtAt' \
+-X 'github.com/Xhofe/alist/conf.GoVersion=$goVersion' \
+-X 'github.com/Xhofe/alist/conf.GitAuthor=$gitAuthor' \
+-X 'github.com/Xhofe/alist/conf.GitCommit=$gitCommit' \
+-X 'github.com/Xhofe/alist/conf.GitTag=$gitTag' \
+"
+go build -ldflags="$ldflags" alist.go
